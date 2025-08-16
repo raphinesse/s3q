61563a411303aeb1fff025826dde64f0eae3acb0
Raphael von der Grün <raphinesse@gmail.com>
2025-08-16 14:36:23 +0000

    test: guard test assertions in release builds

--- tests/classifier_test.cpp
+++ tests/classifier_test.cpp
@@ -5,8 +5,9 @@
 #include <range/v3/core.hpp>
 #include <range/v3/view/iota.hpp>
 
+#include <tlx/die.hpp>
+
 #include <array>
-#include <cassert>
 
 struct TestCfg : s3q::detail::ExtendedCfg<> {
     static constexpr unsigned kLogMaxDegree = 2u;
@@ -20,29 +21,29 @@ int main() {
         int counts[4] = {0};
         classifier.build(std::array{2, 4, 6});
         classifier.classify(ints(1, 9), [&counts](auto cls, auto it) {
-            assert(*it <= (int(cls) + 1) * 2);
+            die_unless(*it <= (int(cls) + 1) * 2);
             ++counts[cls];
         });
-        assert(ranges::all_of(counts, [](auto c) { return c == 2; }));
+        die_unless(ranges::all_of(counts, [](auto c) { return c == 2; }));
     }
 
     { // #buckets = power of two that is less than the max
         int counts[2] = {0, 0};
         classifier.build(std::array{5});
         classifier.classify(ints(1, 11), [&counts](auto cls, auto it) {
-            assert(*it <= (int(cls) + 1) * 5);
+            die_unless(*it <= (int(cls) + 1) * 5);
             ++counts[cls];
         });
-        assert(ranges::all_of(counts, [](auto c) { return c == 5; }));
+        die_unless(ranges::all_of(counts, [](auto c) { return c == 5; }));
     }
 
     { // #buckets = not a power of two
         int counts[3] = {0};
         classifier.build(std::array{3, 6});
         classifier.classify(ints(1, 10), [&counts](auto cls, auto it) {
-            assert(*it <= (int(cls) + 1) * 3);
+            die_unless(*it <= (int(cls) + 1) * 3);
             ++counts[cls];
         });
-        assert(ranges::all_of(counts, [](auto c) { return c == 3; }));
+        die_unless(ranges::all_of(counts, [](auto c) { return c == 3; }));
     }
 }
